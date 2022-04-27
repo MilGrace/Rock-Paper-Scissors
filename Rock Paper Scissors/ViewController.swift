b1d50806757c7243673317b8068f29675e366e0c
Grace <gmilo92@haverfordsd.net>
2022-04-27 10:15:23 -0400

    fixed storyboard + connected all buttons/labels

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBOutlet weak var rockButton: UIButton!
+    @IBAction func rockChosen(_ sender: Any)
+    {
+        
+    }
+    
+    @IBOutlet weak var paperButton: UIButton!
+    @IBAction func paperChosen(_ sender: Any)
+    {
+        
+    }
+    
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBAction func scissorsChosen(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func playChosen(_ sender: Any)
+    {
+        
+    }
+    
+    @IBOutlet weak var youChose: UILabel!
+    @IBOutlet weak var computerChose: UILabel!
+    @IBOutlet weak var wins: UILabel!
+    @IBOutlet weak var losses: UILabel!
+    
+    
+    
 }
 
