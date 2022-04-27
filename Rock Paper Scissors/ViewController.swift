ad6d78405b278ed892987cd6950b26e6b6603a2b
Grace <gmilo92@haverfordsd.net>
2022-04-27 10:29:23 -0400

    Added losses/wins variables + changed icon color backgrounds when selected

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -8,30 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var winCount = 0
+var lossCount = 0
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        rockButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        paperButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        scissorsButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        wins.text = String(winCount)
+        losses.text = String(lossCount)
     }
 
     @IBOutlet weak var rockButton: UIButton!
     @IBAction func rockChosen(_ sender: Any)
     {
-        
+        rockButton.backgroundColor = UIColor.init(red: 0.69, green: 0.89, blue: 0.65, alpha: 1)
+        paperButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        scissorsButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
     }
     
     @IBOutlet weak var paperButton: UIButton!
     @IBAction func paperChosen(_ sender: Any)
     {
-        
+        paperButton.backgroundColor = UIColor.init(red: 0.69, green: 0.89, blue: 0.65, alpha: 1)
+        rockButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        scissorsButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
     }
     
     @IBOutlet weak var scissorsButton: UIButton!
     @IBAction func scissorsChosen(_ sender: Any)
     {
-        
+        scissorsButton.backgroundColor = UIColor.init(red: 0.69, green: 0.89, blue: 0.65, alpha: 1)
+        paperButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
+        rockButton.backgroundColor = UIColor.init(red: 1, green: 0.64, blue: 0.47, alpha: 1)
     }
     
     @IBAction func playChosen(_ sender: Any)
